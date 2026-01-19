7edf8fe61472260fee3a4274166b2278675d9ad1
carterchris571 <85680327+carterchris571@users.noreply.github.com>
2026-01-19 18:08:09 +0100

    Committing 5 items from workspace d65a8c5a-8ab9-47a8-94f6-18a93be16d6e

---
+++ .workspace/Warehouse_DEV_Sales_data.Warehouse/dbo/Tables/Listing_hierachique2.sql
@@ -0,0 +1,13 @@
+CREATE TABLE [dbo].[Listing_hierachique2] (
+
+	[Date modified] datetime2(6) NULL, 
+	[Date] datetime2(6) NULL, 
+	[Status] varchar(8000) NULL, 
+	[Type] varchar(8000) NULL, 
+	[From_num] varchar(8000) NULL, 
+	[From_name] varchar(8000) NULL, 
+	[To_num] varchar(8000) NULL, 
+	[To_name] varchar(8000) NULL, 
+	[Amount] bigint NULL, 
+	[Balance] bigint NULL
+);
